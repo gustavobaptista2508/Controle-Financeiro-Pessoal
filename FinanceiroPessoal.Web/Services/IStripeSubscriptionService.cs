f06b2a5f89e7ce7a26c2f69194ac8bcd5ca4d35c
gustavobaptista2508 <gustavo_hks@hotmail.com>
2026-05-13 17:57:26 -0300

    Corrige contratos Stripe e import do RedirectToAuth

--- FinanceiroPessoal.Web/Services/IStripeSubscriptionService.cs
+++ FinanceiroPessoal.Web/Services/IStripeSubscriptionService.cs
@@ -5,7 +5,8 @@ namespace FinanceiroPessoal.Web.Services;
 public interface IStripeSubscriptionService
 {
     Task<string> CriarCheckoutSessionAsync(int usuarioId, int planoId);
+    Task<string> CriarPortalSessionAsync(int usuarioId);
     Task ProcessarWebhookAsync(string json, string stripeSignature);
-    Task AtualizarAssinaturaPorSubscriptionAsync(Subscription subscription);
-    Task AtualizarAssinaturaPorInvoiceAsync(Invoice invoice);
+    Task AtualizarAssinaturaPorSubscriptionAsync(Stripe.Subscription subscription);
+    Task AtualizarAssinaturaPorInvoiceAsync(Stripe.Invoice invoice);
 }
