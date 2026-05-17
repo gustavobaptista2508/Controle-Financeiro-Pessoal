45d0100853d5d51adb920d82e3319d836b06ad57
Luiz Gustavo Carvalho Baptista <gustavo_hks@hotmail.com>
2026-05-16 21:08:21 -0300

    Corrige injeção de contexto nos repositórios Core

--- FinanceiroPessoal.Core/Services/DatabaseFactory.cs
+++ FinanceiroPessoal.Core/Services/DatabaseFactory.cs
@@ -1,30 +1,25 @@
-﻿using FinanceiroPessoal.Core.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using FinanceiroPessoal.Core.Data;
+using FinanceiroPessoal.Core.Repositories;
 
 namespace FinanceiroPessoal.Core.Services
 {
-    public static class DatabaseFactory
+    public class DatabaseFactory
     {
-        public static ILancamentoRepository CriarLancamentoRepository(TipoBanco tipo)
+        private readonly FinanceiroDbContext _context;
+
+        public DatabaseFactory(FinanceiroDbContext context)
         {
-            return tipo switch
-            {
-                TipoBanco.LocalSqlite => new SqliteLancamentoRepository(),
-                TipoBanco.OnlineMySql => new MySqlLancamentoRepository(),
-                _ => new SqliteLancamentoRepository()
-            };
+            _context = context;
         }
 
-        public static ICadastroAuxiliarRepository CriarCadastroAuxiliarRepository(TipoBanco tipo)
+        public ILancamentoRepository CriarLancamentoRepository(TipoBanco tipo = TipoBanco.OnlineMySql)
         {
-            return tipo switch
-            {
-                TipoBanco.LocalSqlite => new SqliteCadastroAuxiliarRepository(),
-                TipoBanco.OnlineMySql => new MySqlCadastroAuxiliarRepository(),
-                _ => new SqliteCadastroAuxiliarRepository()
-            };
-        } 
+            return new MySqlLancamentoRepository(_context);
+        }
+
+        public ICadastroAuxiliarRepository CriarCadastroAuxiliarRepository(TipoBanco tipo = TipoBanco.OnlineMySql)
+        {
+            return new MySqlCadastroAuxiliarRepository(_context);
+        }
     }
 }
