c6c6a5e4587eb327030aa589037f73eac00720f4
Luiz Gustavo Carvalho Baptista <gustavo_hks@hotmail.com>
2026-08-16 23:54:19 -0300

    build: bump GranaOk beta badge to 0.3.8

--- GranaOk/android/app/src/main/assets/www/safe-version.js
+++ GranaOk/android/app/src/main/assets/www/safe-version.js
@@ -1,5 +1,5 @@
 (()=>{
-  const version='BETA 0.3.7';
+  const version='BETA 0.3.8';
   try{
     window.brand=function(){
       return '<div class="brandrow"><span class="logo-mark"></span><div class="brand">Grana<span>Ok</span><small>'+version+'</small></div></div>';
