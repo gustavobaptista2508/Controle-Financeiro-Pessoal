446509c2f0a516ad8cd6d4cc82736f58c9315d69
Luiz Gustavo Carvalho Baptista <gustavo_hks@hotmail.com>
2026-08-17 10:21:05 -0300

    ui: label GranaOk beta 0.5.2

--- GranaOk/android/app/src/main/assets/www/safe-version.js
+++ GranaOk/android/app/src/main/assets/www/safe-version.js
@@ -1,5 +1,5 @@
 (()=>{
-  const version='BETA 0.5.1';
+  const version='BETA 0.5.2';
   try{
     window.brand=function(){
       return '<div class="brandrow"><span class="logo-mark"></span><div class="brand">Grana<span>Ok</span><small>'+version+'</small></div></div>';
