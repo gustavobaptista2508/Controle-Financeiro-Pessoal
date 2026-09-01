52afb68340c236bbcd43c854c07ef2d834b437df
Luiz Gustavo Carvalho Baptista <gustavo_hks@hotmail.com>
2026-09-01 10:37:27 -0300

    desktop: expose secure renderer bridge

---
+++ GranaOk/desktop/preload.js
@@ -0,0 +1,7 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('granaok', {
+  invoke: (action, payload = {}) => ipcRenderer.invoke('granaok:invoke', action, payload),
+  platform: process.platform,
+  version: '0.1.0'
+});
