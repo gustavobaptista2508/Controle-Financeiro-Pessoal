a08ce3a156f6931ff4bc8367802fb970371ace18
Luiz Gustavo Carvalho Baptista <gustavo_hks@hotmail.com>
2026-09-01 20:02:40 -0300

    desktop: bump bridge version 0.1.3

--- GranaOk/desktop/preload.js
+++ GranaOk/desktop/preload.js
@@ -3,5 +3,5 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('granaok', {
   invoke: (action, payload = {}) => ipcRenderer.invoke('granaok:invoke', action, payload),
   platform: process.platform,
-  version: '0.1.2'
+  version: '0.1.3'
 });
